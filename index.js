f80ac97cd73f8ff4c8158abe44a4fa670f279de8
dvlprJobayer <nesarahammed019@gmail.com>
2022-05-22 20:05:41 +0600

    mongodb connected

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// Make App
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bvzh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+// Test Api
+app.get('/', (req, res) => {
+    res.send('Running to do server');
+});
+
+// Listen App
+app.listen(port, () => {
+    console.log('Running server on', port);
+});
