afef542bc95cd5fc7a0c2604d02ba50da027ef18
dvlprJobayer <nesarahammed019@gmail.com>
2022-05-23 14:27:38 +0600

    get token api done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,12 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.get('/get-token', (req, res) => {
+    const { email } = req.body;
+    const token = jwt.sign({ email }, process.env.TOKEN_SECRET);
+    res.send({ success: true, accessToken: token });
+});
 
 // Database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bvzh.mongodb.net/?retryWrites=true&w=majority`;
