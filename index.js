eb54916fed10ca2d133a12d252af321490ac269e
dvlprJobayer <nesarahammed019@gmail.com>
2022-05-23 20:16:55 +0600

    delete user api done

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ app.get('/get-token', (req, res) => {
     res.send({ success: true, accessToken: token });
 });
 
-app.use((req, res, next) => {
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader) {
         return res.status(401).send({ message: 'unauthorized access' });
     }
     const token = authHeader.split(' ')[1];
+    console.log(token);
     jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
         if (err) {
             return res.status(403).send({ message: 'forbidden access' });
@@ -31,7 +32,7 @@ app.use((req, res, next) => {
         req.decoded = decoded;
         next();
     });
-})
+}
 
 // Database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bvzh.mongodb.net/?retryWrites=true&w=majority`;
@@ -62,7 +63,7 @@ async function run() {
         });
 
         // Get All user
-        app.get('/user', async (req, res) => {
+        app.get('/user', verifyJWT, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
@@ -77,6 +78,14 @@ async function run() {
                 }
             };
             const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        // Delete User
+        app.patch('/user/:id', async (req, res) => {
+            const { id } = req.params;
+            const filter = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(filter);
             res.send(result);
         });
     }
